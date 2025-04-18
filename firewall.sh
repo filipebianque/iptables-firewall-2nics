e9a3ca55203a7eae8cca4cbb6ba76fa3124389cb
Filipe Bianque <filipebianque@gmail.com>
2025-04-18 17:00:44 -0300

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,57 +1,87 @@
 #!/bin/bash
 
-# Interfaces de rede
-INTERNET_IFACE="eth0"
-LAN_IFACE="eth1"
-LAN_SUBNET="192.168.0.0/24"
+### FIREWALL IPTABLES AVANÇADO
+### Ambiente: Servidor com 2 interfaces de rede (ex: eth0 = internet, eth1 = rede interna)
 
-# Resetando regras
+# Defina as interfaces de rede
+IF_EXT="eth0"   # Interface externa (Internet)
+IF_INT="eth1"   # Interface interna (LAN)
+
+# IP da rede interna
+REDE_INT="192.168.0.0/24"
+
+# Flush nas regras atuais
 iptables -F
 iptables -X
 iptables -t nat -F
-iptables -t nat -X
+iptables -t mangle -F
 
-# Politicas padrão
+# Política padrão
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
-# Habilita roteamento
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# NAT para compartilhamento de internet
-iptables -t nat -A POSTROUTING -o $INTERNET_IFACE -j MASQUERADE
-iptables -A FORWARD -i $LAN_IFACE -o $INTERNET_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $INTERNET_IFACE -o $LAN_IFACE -j ACCEPT
-
-# Libera localhost e LAN
+# Libera loopback
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -i $LAN_IFACE -s $LAN_SUBNET -j ACCEPT
 
-# Bloqueio de IPs externos
-if [ -f rules/blacklist.txt ]; then
-  for ip in $(cat rules/blacklist.txt); do
-    iptables -A INPUT -s $ip -j DROP
-  done
-fi
+# Libera conexões já estabelecidas e relacionadas
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Bloqueio de DNS externo (exceto IPs permitidos)
-iptables -A OUTPUT -p udp --dport 53 -j DROP
-if [ -f rules/dns_allowed.txt ]; then
-  for ip in $(cat rules/dns_allowed.txt); do
-    iptables -I OUTPUT -d $ip -p udp --dport 53 -j ACCEPT
-  done
-fi
+# Libera acesso SSH (ajuste a porta conforme necessário)
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# Redirecionamento de portas
-if [ -f rules/port_forwarding.txt ]; then
-  while read line; do
-    EXT_PORT=$(echo $line | cut -d':' -f1)
-    DEST_IP=$(echo $line | cut -d':' -f2)
-    DEST_PORT=$(echo $line | cut -d':' -f3)
-    iptables -t nat -A PREROUTING -p tcp --dport $EXT_PORT -j DNAT --to-destination $DEST_IP:$DEST_PORT
-    iptables -A FORWARD -p tcp -d $DEST_IP --dport $DEST_PORT -j ACCEPT
-  done < rules/port_forwarding.txt
-fi
+# NAT - Compartilhamento de Internet
+iptables -t nat -A POSTROUTING -s "$REDE_INT" -o "$IF_EXT" -j MASQUERADE
+iptables -A FORWARD -i "$IF_INT" -o "$IF_EXT" -s "$REDE_INT" -j ACCEPT
+iptables -A FORWARD -i "$IF_EXT" -o "$IF_INT" -d "$REDE_INT" -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-echo "Firewall carregado com sucesso."
+# BLOQUEIO DE SITES EXTERNOS (exemplos)
+# Bloqueia facebook
+iptables -A FORWARD -p tcp -d facebook.com -j REJECT
+iptables -A FORWARD -p tcp -d www.facebook.com -j REJECT
+# Bloqueia TikTok (via IP ou range conhecido)
+iptables -A FORWARD -p tcp -d 161.117.57.0/24 -j REJECT
+
+# BLOQUEIO DE IPs EXTERNOS MALICIOSOS
+iptables -A INPUT -s 45.134.20.0/24 -j DROP   # Exemplo de IP malicioso
+iptables -A INPUT -s 185.234.219.0/24 -j DROP
+
+# REDIRECIONAMENTO DE PORTAS
+# Exemplo: redireciona porta 80 do firewall para servidor interno 192.168.0.10 porta 80
+iptables -t nat -A PREROUTING -i "$IF_EXT" -p tcp --dport 80 -j DNAT --to-destination 192.168.0.10:80
+iptables -A FORWARD -p tcp -d 192.168.0.10 --dport 80 -j ACCEPT
+
+# BLOQUEIO DE DNS EXTERNO
+iptables -A FORWARD -p udp --dport 53 -j REJECT
+iptables -A FORWARD -p tcp --dport 53 -j REJECT
+
+# EXCEÇÕES AO BLOQUEIO DE DNS PARA IPs INTERNOS ESPECÍFICOS
+iptables -A FORWARD -s 192.168.0.2 -p udp --dport 53 -j ACCEPT   # Libera para IP específico
+iptables -A FORWARD -s 192.168.0.3 -p tcp --dport 53 -j ACCEPT
+
+# PRIORIZAÇÃO DE TRÁFEGO (QOS SIMPLES COM MARK)
+# Marcar pacotes HTTP/HTTPS/FTP/IMAP da diretoria para tratamento especial
+# Lista de IPs da diretoria:
+IP_DIRETORIA="192.168.0.2 192.168.0.3"
+
+for ip in $IP_DIRETORIA; do
+  iptables -t mangle -A PREROUTING -s $ip -p tcp --dport 80 -j MARK --set-mark 10  # HTTP
+  iptables -t mangle -A PREROUTING -s $ip -p tcp --dport 443 -j MARK --set-mark 10 # HTTPS
+  iptables -t mangle -A PREROUTING -s $ip -p tcp --dport 21 -j MARK --set-mark 10  # FTP
+  iptables -t mangle -A PREROUTING -s $ip -p tcp --dport 143 -j MARK --set-mark 10 # IMAP
+  iptables -t mangle -A PREROUTING -s $ip -p tcp --dport 993 -j MARK --set-mark 10 # IMAPS
+  iptables -t mangle -A PREROUTING -s $ip -p tcp --dport 587 -j MARK --set-mark 10 # SMTP Auth
+
+done
+
+# Aqui você pode integrar com tc/qdisc se desejar priorização real via traffic control
+
+# LOG de pacotes descartados (opcional)
+iptables -A INPUT -j LOG --log-prefix "[IPTABLES DROP INPUT] " --log-level 4
+iptables -A FORWARD -j LOG --log-prefix "[IPTABLES DROP FORWARD] " --log-level 4
+
+# Fim
+clear
+echo "Firewall aplicado com sucesso."
+iptables -L -v -n
